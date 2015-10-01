df5f0290eca4244503b77c7750fce347fcd7fc92
GuidoCeresa <gac@algos.it>
2015-10-01 11:16:07 +0200

    .

--- src/it/algos/webbase/web/servlet/AlgosServlet.java
+++ src/it/algos/webbase/web/servlet/AlgosServlet.java
@@ -1,112 +1,60 @@
-package it.algos.evento.login;
+package it.algos.webbase.web.servlet;
 
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.Window;
-import it.algos.webbase.domain.utente.Utente;
-import it.algos.webbase.web.dialog.ConfirmDialog;
-import it.algos.webbase.web.field.CheckBoxField;
-import it.algos.webbase.web.field.PasswordField;
-import it.algos.webbase.web.field.TextField;
-import it.algos.webbase.web.form.AFormLayout;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.*;
+import it.algos.webbase.web.lib.Cost;
+import it.algos.webbase.web.lib.LibSession;
+import it.algos.webbase.web.ui.AlgosUI;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 
 /**
- * Base Login form (UI).
+ * Servlet 3.0 introduces a @WebServlet annotation which can be used to replace the traditional web.xml.
+ * <p>
+ * The straightforward approach to create a Vaadin application using servlet 3.0 annotations,
+ * is to simply move whatever is in web.xml to a custom servlet class (extends VaadinServlet)
+ * and annotate it using @WebServlet and add @WebInitParams as needed.
+ * <p><
+ * Vaadin 7.1 introduces two features which makes this a lot easier, @VaadinServletConfiguration
+ * and automatic UI finding.
+ * VaadinServletConfiguration is a type safe, Vaadin version of @WebInitParam
+ * which provides you with the option to select UI by referring the UI class
+ * directly toggle productionMode using a boolean and more
  */
-public class BaseLoginForm extends ConfirmDialog implements LoginForm {
-
-    private TextField nameField;
-    private PasswordField passField;
-    private CheckBoxField rememberField;
-    private LoginListener loginListener;
-
-    /**
-     * Constructor
-     */
-    public BaseLoginForm() {
-        super(null);
-        init();
-    }// end of constructor
-
-    /**
-     * Initialization <br>
-     */
-    private void init() {
-        FormLayout layout = new AFormLayout();
-        layout.setSpacing(true);
-
-        // crea i campi
-        nameField = new TextField("Username");
-        nameField.setWidthUndefined();
-        passField = new PasswordField("Password");
-        passField.setWidthUndefined();
-        rememberField=new CheckBoxField("Ricordami su questo computer");
-
-        // aggiunge i campi al layout
-        layout.addComponent(nameField);
-        layout.addComponent(passField);
-        layout.addComponent(rememberField);
-
-        addComponent(layout);
-    }// end of method
-
-
+public abstract class AlgosServlet extends VaadinServlet implements SessionInitListener, SessionDestroyListener {
 
     @Override
-    protected void onConfirm() {
-        String nome = nameField.getValue();
-        String password = passField.getValue();
-        Utente utente = Utente.validate(nome, password);
-        if (utente != null) {
-            super.onConfirm();
-            utenteLoggato(utente);
-        } else {
-            Notification.show("Login fallito", Notification.Type.WARNING_MESSAGE);
-        }// end of if/else cycle
+    protected void servletInitialized() throws ServletException {
+        super.servletInitialized();
+        getService().addSessionInitListener(this);
+        getService().addSessionDestroyListener(this);
     }// end of method
 
-
     /**
-     * Evento generato quando si modifica l'utente loggato <br>
+     * Invoked when a new Vaadin service session is initialized for that service.
      * <p>
-     * Informa (tramite listener) chi è interessato <br>
+     * Because of the way different service instances share the same session, the listener is not necessarily notified immediately
+     * when the session is created but only when the first request for that session is handled by a specific service.
+     *
+     * @param event the initialization event
+     * @throws ServiceException a problem occurs when processing the event
      */
-    private void utenteLoggato(Utente utente) {
-        if(loginListener!=null){
-            loginListener.onUserLogin(utente, rememberField.getValue());
-        }
-    }
-
     @Override
-    public void setLoginListener(LoginListener listener) {
-        this.loginListener = listener;
-    }
+    public void sessionInit(SessionInitEvent event) throws ServiceException {
+        // Do session start stuff here
+        LibSession.setDeveloper(false);
+    }// end of method
 
+    /**
+     * Called when a Vaadin service session is no longer used.
+     *
+     * @param event the event with details about the destroyed session
+     */
     @Override
-    public Window getWindow() {
-        return this;
-    }
+    public void sessionDestroy(SessionDestroyEvent event) {
+        // Do session end stuff here
+    }// end of method
 
-    @Override
-    public void setUsername(String name) {
-        nameField.setValue(name);
-    }
-
-    @Override
-    public void setPassword(String password) {
-        passField.setValue(password);
-    }
-
-    @Override
-    public void setRemember(boolean remember) {
-        rememberField.setValue(remember);
-    }
-
-    @Override
-    public boolean isRemember() {
-        return rememberField.getValue();
-    }
-
-
-}
-
+}// end of abstract class

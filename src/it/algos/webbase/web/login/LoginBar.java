ac506d23976f0c364e4752fdd0e24f2d91a005b1
GuidoCeresa <gac@algos.it>
2015-10-02 17:45:19 +0200

    .

--- src/it/algos/webbase/web/login/LoginBar.java
+++ src/it/algos/webbase/web/login/LoginBar.java
@@ -16,12 +16,7 @@ public class LoginBar extends HorizontalLayout implements LoginListener {
 
     private static String TESTO_NON_LOGGATO = "Loggato come Anonymous";
     private static boolean USA_TESTO = false;
-    private MenuBar menubar;
-    //    private ArrayList<LogBottoniListener> listeners = new ArrayList<LogBottoniListener>();
-    private Utente utente;
-    private Label label;
-    private Button buttonLogin;
-    private Button buttonEsci;
+     MenuBar menubar;
     private MenuBar.MenuItem loginItem; // il menuItem di login
 
     /**
@@ -54,55 +49,13 @@ public class LoginBar extends HorizontalLayout implements LoginListener {
         this.setHeight("40px");
         this.setWidth("100%");
 
-        // this.setMargin(true);
-        // this.setSpacing(true);
-//		this.setHeight("10%");
-//		this.setWidth("100%");
-
-        // si autoregistra per rispondere al menu/bottone
-//        this.addLogBottoneListener(this);
         menubar = new MenuBar();
         loginItem = menubar.addItem("Login1", null, null);
         this.addComponent(menubar);
         this.setComponentAlignment(menubar, Alignment.MIDDLE_RIGHT);
-//        creaLabelAndBottoni();
 
-//        elaboraComponenti();
         updateLoginUI();
     }// end of method
-
-//    /**
-//     * Creazione dei componenti grafici
-//     */
-//    private void creaLabelAndBottoni() {
-//        this.label = new Label();
-//        this.label.setSizeUndefined();
-//        buttonLogin = creaBottoneLogin();
-//        buttonEsci = creaBottoneEsci();
-//    }// end of method
-
-//    /**
-//     * Visualizzazione dei componenti
-//     */
-//    private void barraLoggato(Button bottoneAttivo) {
-//        HorizontalLayout layout;
-//
-//        if (USA_TESTO) {
-//            layout = new HorizontalLayout();
-//            layout.setSpacing(true);
-//            layout.addComponent(label);
-//            layout.addComponent(bottoneAttivo);
-//            layout.setComponentAlignment(label, Alignment.MIDDLE_LEFT);
-//            layout.setComponentAlignment(bottoneAttivo, Alignment.MIDDLE_RIGHT);
-//            this.addComponent(layout);
-//            this.setComponentAlignment(layout, Alignment.MIDDLE_RIGHT);
-//        } else {
-//            this.addComponent(bottoneAttivo);
-//            this.setComponentAlignment(bottoneAttivo, Alignment.MIDDLE_RIGHT);
-//        }// end of if/else cycle
-//
-//    }// end of method
-
 
     /**
      * Aggiorna la UI di bottone/menu in base ai contenuti della session
@@ -117,7 +70,7 @@ public class LoginBar extends HorizontalLayout implements LoginListener {
                 @Override
                 public void menuSelected(MenuBar.MenuItem selectedItem) {
                     loginCommandSelected();
-                }
+                }// end of method
             });
 
         } else {
@@ -130,127 +83,13 @@ public class LoginBar extends HorizontalLayout implements LoginListener {
                 @Override
                 public void menuSelected(MenuBar.MenuItem selectedItem) {
                     logoutCommandSelected();
-                }
+                }// end of method
             });
 
-        }
+        }// end of if/else cycle
 
     }// end of method
 
-//    /**
-//     * Visualizzazione da loggato
-//     */
-//    private void barraLoggato() {
-//        this.barraLoggato(buttonEsci);
-//    }// end of method
-//
-//    /**
-//     * Visualizzazione da sloggato
-//     */
-//    private void barraNonLoggato() {
-//        this.barraLoggato(buttonLogin);
-//    }// end of method
-
-
-    /**
-     * Evento generato dal bottone di comando <br>
-     * <p>
-     * Informa (tramite listener) chi è interessato <br>
-     */
-    protected void bottoneEsciPremuto() {
-//        for (LogBottoniListener listener : listeners) {
-//            listener.esciLogin();
-//        }// end of for cycle
-    }// end of method
-
-//    /**
-//     * Visualizza un bottone di comando <br>
-//     */
-//    private Button creaBottoneLogin() {
-//        Button button = new Button("Login");
-//
-//        button.addClickListener(new Button.ClickListener() {
-//            @Override
-//            public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
-//                bottoneLoginPremuto();
-//            }// end of inner method
-//        });// end of anonymous class
-//
-//        button.setSizeUndefined();
-//        return button;
-//    }// end of method
-
-//    /**
-//     * Visualizza un bottone di comando <br>
-//     */
-//    private Button creaBottoneEsci() {
-//        Button button = new Button("Esci");
-//
-//        button.addClickListener(new Button.ClickListener() {
-//            @Override
-//            public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
-//                bottoneEsciPremuto();
-//            }// end of inner method
-//        });// end of anonymous class
-//
-//        button.setSizeUndefined();
-//        return button;
-//    }// end of method
-
-//    /**
-//     * Listener notificato quando si preme il bottone login
-//     */
-//    public void addLogBottoneListener(LogBottoniListener listener) {
-//        listeners.add(listener);
-//    }// end of method
-//
-//    /**
-//     * Azione effettuata quando si preme il bottone login <br>
-//     * Passa il controllo a LoginLogic <br>
-//     */
-//    @Override
-//    public void openLogin() {
-//        LoginLogic loginLogic = new LoginLogic(UI.getCurrent());
-//        loginLogic.addUtenteLoginListener(this);
-//    }// end of method
-//
-//    /**
-//     * Azione effettuata quando si preme il bottone esci <br>
-//     */
-//    @Override
-//    public void esciLogin() {
-//        this.utente = null;
-//        this.reset();
-//    }// end of method
-
-//    /**
-//     * Crea la componente grafica <br>
-//     */
-//    public void elaboraComponenti() {
-//        if (utente != null) {
-//            label.setCaption("Loggato come: " + utente.getNickname());
-//            barraLoggato();
-//        } else {
-//            label.setCaption(TESTO_NON_LOGGATO);
-//            barraNonLoggato();
-//        }// end of if/else cycle
-//    }// end of method
-
-//    /**
-//     * Azzera l'aspetto grafico e lo ricrea <br>
-//     */
-//    public void reset() {
-//        this.removeAllComponents();
-//        elaboraComponenti();
-//    }// end of method
-
-    /**
-     * Azione effettuata quando si modifica l'utente loggato <br>
-     */
-    public void setUtenteLogin(Utente utente) {
-        this.utente = utente;
-//        this.reset();
-    }// end of method
 
     /**
      * Logout

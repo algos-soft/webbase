4a819379d80860fe68fef7daaa59767d232ae57f
algos-soft <info@algos.it>
2016-07-29 13:11:31 +0200

    .

--- src/it/algos/webbase/web/component/Spacer.java
+++ src/it/algos/webbase/web/component/Spacer.java
@@ -8,43 +8,44 @@ import com.vaadin.ui.Label;
 @SuppressWarnings("serial")
 public class Spacer extends Label {
 
-	/**
-	 * @param width
-	 *            the width in EM
-	 * @param heigth
-	 *            the height in EM
-	 * @param unit
-	 *            the unit (com.vaadin.server.Sizeable.Unit)
-	 */
-	public Spacer(int width, int heigth, Unit unit) {
-		super();
-		setWidth(width, unit);
-		setHeight(heigth, unit);
-	}
-	
-	/**
-	 * @param size
-	 *            the size in EM
-	 */
-	public Spacer(int size, Unit unit) {
-		this(size, size, unit);
-	}
+    /**
+     * Create a generic Spacer.
+     *
+     * @param width  the width
+     * @param heigth the height
+     * @param unit   the unit (com.vaadin.server.Sizeable.Unit)
+     */
+    public Spacer(int width, int heigth, Unit unit) {
+        super();
+        setWidth(width, unit);
+        setHeight(heigth, unit);
+    }
 
-	/**
-	 * @param size
-	 *            the size in EM
-	 */
-	public Spacer(int size) {
-		this(size, Unit.PIXELS);
-	}
+    /**
+     * Create a square Spacer with size in a given unit.
+     *
+     * @param size the size in EM (same for width & height)
+     * @param unit the unit (com.vaadin.server.Sizeable.Unit)
+     */
+    public Spacer(int size, Unit unit) {
+        this(size, size, unit);
+    }
 
-	/**
-	 */
-	public Spacer() {
-		this(20);
-	}
+    /**
+     * Create a square Spacer.
+     *
+     * @param size the size in EM
+     */
+    public Spacer(int size) {
+        this(size, Unit.EM);
+    }
 
-
+    /**
+     * Create a default Spacer, 1em for width & height.
+     */
+    public Spacer() {
+        this(1);
+    }
 
 
 }
